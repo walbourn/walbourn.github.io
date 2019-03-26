1fafbcd767d00cc4fcba76aba42cd15978779bac
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-03-26 16:53:39 -0700

    Script for creating new posts

---
+++ newpost.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+    param (
+        # Name
+        [Parameter(Mandatory)]
+        [string]
+        $Name 
+    )
+    
+    process {
+        $Date = Get-Date -Format "yyyy-MM-dd"
+        $Time = Get-Date -Format "HH:MM"
+        $FileName = "$($date.ToString())-$($name.Replace(" ","-")).md"
+
+        $Blog = New-Object -TypeName "System.Text.StringBuilder"
+        [void]$Blog.Append("---`n") 
+        [void]$Blog.Append("layout: post`n")
+        [void]$Blog.Append("title: $Name`n") 
+        [void]$Blog.Append("date: $Date $Time`n") 
+        [void]$Blog.Append("author: Chuck Walbourn`n") 
+        [void]$Blog.Append("comments: true`n") 
+        [void]$Blog.Append("categories: [general]`n") 
+        [void]$Blog.Append("---`n") 
+        [void]$Blog.Append($Content)
+        $Blog = $Blog.toString() 
+
+        $NewBlogPath = Join-Path -ChildPath $FileName -Path "./_posts"
+
+        Set-Content -Value $Blog -Path $NewBlogPath
+        Write-Output (Get-ChildItem $NewBlogPath)
+
+        atom $NewBlogPath
+    }
+    
+    end {
+    }
